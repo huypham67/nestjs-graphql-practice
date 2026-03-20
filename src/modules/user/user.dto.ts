3396cd958b5eab3311ba6de353f72f274ef667c6
huypham67 <huypdn6703@gmail.com>
2026-03-20 23:09:07 +0700

    refactor: update User model to use Date type for createdAt and adjust GraphQL schema accordingly

--- src/modules/user/user.dto.ts
+++ src/modules/user/user.dto.ts
@@ -1,4 +1,10 @@
-import { Field, InputType, Int, ObjectType } from '@nestjs/graphql';
+import {
+  Field,
+  InputType,
+  Int,
+  ObjectType,
+  PartialType,
+} from '@nestjs/graphql';
 
 @ObjectType()
 export class User {
@@ -15,25 +21,20 @@ export class User {
   password: string;
 
   @Field()
-  createdAt: string;
+  createdAt: Date;
 }
 
 @InputType()
 export class CreateUserInput {
   @Field()
   name: string;
+
   @Field()
   email: string;
+
   @Field()
   password: string;
 }
 
 @InputType()
-export class UpdateUserInput {
-  @Field({ nullable: true })
-  name: string;
-  @Field({ nullable: true })
-  email: string;
-  @Field({ nullable: true })
-  password: string;
-}
+export class UpdateUserInput extends PartialType(CreateUserInput) {}
